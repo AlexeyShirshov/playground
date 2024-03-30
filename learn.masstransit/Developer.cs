dcf83ebc1ee4c66b4064a78900309f772b6c0f9f
Alexey Shirshov <alex.shirshov@vk.com>
2024-03-30 10:58:11 +0500

    play with sagas

---
+++ learn.masstransit/Developer.cs
@@ -0,0 +1,166 @@
+﻿using MassTransit;
+
+namespace learn.masstransit;
+
+public class Developer : MassTransitStateMachine<TheDeveloper>
+{
+    public Developer()
+    {
+        InstanceState(x => x.CurrentState,
+            Performing, // 3
+            Sleeping // 4
+        );
+
+        Initially(
+            When(Awake)
+                .ThenAsync(async (developerCtx) =>
+                {
+                    var developer = developerCtx.Saga;
+                    Console.WriteLine($"{developer.Id} was in {developer.State} state, now start performing");
+                    await developerCtx.TransitionToState(Performing);
+                    Task.Run(async () =>
+                    {
+                        await Task.Delay(5000);
+                        await developerCtx.Publish<ITiredEvent>(new
+                        {
+                            developer.Id,
+                        });
+                    });
+                })
+                .Respond(messageFactory => messageFactory.Saga)
+        );
+
+        During(Performing,
+            When(Tired)
+                .ThenAsync(async (developerCtx) =>
+                {
+                    var developer = developerCtx.Saga;
+                    developer.SleepCount++;
+
+                    if (developer.SleepCount >= 2)
+                    {
+                        Console.WriteLine($"{developer.Id} going home");
+                        await developerCtx.TransitionToState(Final);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{developer.Id} was in {developer.State} state, now is tired, so going to sleep");
+                        await developerCtx.TransitionToState(Sleeping);
+                    }
+                }),
+            When(Awake)
+                .Then((developerCtx) =>
+                {
+                    var developer = developerCtx.Saga;
+                    Console.WriteLine($"{developer.Id} already performing, don't disturb him");
+                })
+                .Respond(messageFactory => messageFactory.Saga)
+        );
+
+        During(Sleeping,
+            When(Awake, developerCtx => developerCtx.Saga.SleepCount < 2)
+                .ThenAsync(async (developerCtx) =>
+                {
+                    var developer = developerCtx.Saga;
+                    Console.WriteLine($"{developer.Id} was in {developer.State} state, now start performing");
+                    await developerCtx.TransitionToState(Performing);
+                    Task.Run(async () =>
+                    {
+                        await Task.Delay(5000);
+                        await developerCtx.Publish<ITiredEvent>(new
+                        {
+                            developer.Id,
+                        });
+                    });
+                })
+                .Respond(messageFactory => messageFactory.Saga)
+        );
+
+        During(Final,
+            When(Awake)
+                .Then(developerCtx =>
+                {
+                    var developer = developerCtx.Saga;
+                    Console.WriteLine($"{developer.Id} is in home");
+                })
+                .Respond(messageFactory => messageFactory.Saga),
+            When(Tired)
+                .Then(developerCtx =>
+                {
+                    var developer = developerCtx.Saga;
+                    Console.WriteLine($"{developer.Id} is in home");
+                })
+                .Respond(messageFactory => messageFactory.Saga),
+            When(NewDay)
+                .Then(developerCtx =>
+                {
+                    var developer = developerCtx.Saga;
+                    developer.SleepCount = 0;
+                    Console.WriteLine($"{developer.Id} have a rest and ready to work");
+                })
+                .TransitionTo(Initial)
+                .Respond(messageFactory => messageFactory.Saga),
+            When(HowYDoing)
+                .Respond(messageFactory => messageFactory.Saga)
+        );
+
+        DuringAny(
+            When(HowYDoing)
+                .Respond(messageFactory => messageFactory.Saga)
+        );
+        Event(() => Awake, config => config.CorrelateById(theDeveloper => theDeveloper.Message.Id));
+        Event(() => Tired, config => config.CorrelateById(theDeveloper => theDeveloper.Message.Id));
+        Event(() => NewDay, config => config.CorrelateById(theDeveloper => theDeveloper.Message.Id));
+        Event(() => HowYDoing, config =>
+        {
+            config.CorrelateById(theDeveloper => theDeveloper.Message.Id);
+            config.ReadOnly = true;
+            config.OnMissingInstance(behavior => behavior.Execute(context =>
+            {
+                context.Respond(new UnknownDeveloper());
+            }));
+        });
+    }
+    /// <summary>
+    /// 3
+    /// </summary>
+    public State? Performing { get; set; }
+    /// <summary>
+    /// 4
+    /// </summary>
+    public State? Sleeping { get; set; }
+    public Event<IAwakeEvent>? Awake { get; private set; }
+    public Event<ITiredEvent>? Tired { get; private set; }
+    public Event<INewDayEvent>? NewDay { get; private set; }
+    public Event<IHowYDoing>? HowYDoing { get; private set; }
+}
+public class TheDeveloper : SagaStateMachineInstance, ISagaVersion
+{
+    public Guid CorrelationId { get; set; }
+    public Guid Id => CorrelationId;
+    public int CurrentState { get; set; }
+    public DeveloperState State { get => (DeveloperState)CurrentState; }
+    public int SleepCount { get; set; }
+    public int Version { get; set; }
+}
+public class UnknownDeveloper { }
+public class DeveloperStatus
+{
+    public int State { get; set; }
+    public int SleepCount { get; set; }
+}
+public interface IDeveloperEvent
+{
+    Guid Id { get; set; }
+}
+public interface IAwakeEvent : IDeveloperEvent { }
+public interface ITiredEvent : IDeveloperEvent { }
+public interface INewDayEvent : IDeveloperEvent { }
+public interface IHowYDoing : IDeveloperEvent { }
+public enum DeveloperState
+{
+    Initial = 1,
+    Final = 2,
+    Performing = 3,
+    Sleeping = 4
+}
---
+++ learn.masstransit/Program.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using learn.masstransit;
+using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddMassTransit(busConfig =>
+{
+    var currentAssembly = Assembly.GetExecutingAssembly();
+
+    busConfig.AddSagas(currentAssembly);
+    busConfig.AddSagaStateMachine<Developer, TheDeveloper>()
+        .InMemoryRepository();
+
+    busConfig.AddRequestClient<IRequestClient<IHowYDoing>>();
+    busConfig.AddRequestClient<IRequestClient<IAwakeEvent>>();
+
+    busConfig.UsingInMemory((context, inMemoryConfig) =>
+    {
+        inMemoryConfig.ConfigureEndpoints(context);
+    });
+});
+
+services.AddLogging(config =>
+{
+    config.SetMinimumLevel(LogLevel.Trace);
+    config.AddFilter((ctx, _) => ctx?.StartsWith("Microsoft.AspNetCore.Server.Kestrel") != true
+        && ctx?.StartsWith("Microsoft.AspNetCore.Routing.EndpointMiddleware") != true
+        && ctx?.StartsWith("Microsoft.AspNetCore.Routing.EndpointRoutingMiddleware") != true
+        && ctx?.StartsWith("Microsoft.AspNetCore.HostFiltering.HostFilteringMiddleware") != true
+        && ctx?.StartsWith("Microsoft.AspNetCore.Routing.Matching.DfaMatcher") != true
+    );
+    config.AddSimpleConsole(consoleConfig =>
+    {
+        consoleConfig.SingleLine = true;
+        consoleConfig.TimestampFormat = "[yyyy-MM-dd HH:mm:ss.fff] ";
+    });
+});
+
+var app = builder.Build();
+
+app.MapGet("awake/{id}", async (Guid id, IBus bus, IRequestClient<IAwakeEvent> awake) =>
+{
+    // await bus.Publish<IAwakeEvent>(new
+    // {
+    //     Id = id
+    // });
+
+    var developer = awake.GetResponse<TheDeveloper>(new
+    {
+        Id = id
+    });
+
+    var options = new JsonSerializerOptions();
+    options.Converters.Add(new JsonStringEnumConverter());
+    return Results.Json((await developer).Message, options);
+});
+
+app.MapGet("newday/{id}", async (Guid id, IBus bus) =>
+{
+    await bus.Publish<INewDayEvent>(new
+    {
+        Id = id
+    });
+});
+
+app.MapGet("howydoing/{id}", async (Guid id, IRequestClient<IHowYDoing> howYDoing) =>
+{
+    var (developer, _) = await howYDoing.GetResponse<TheDeveloper, UnknownDeveloper>(new
+    {
+        Id = id
+    });
+
+    if (developer.IsCompletedSuccessfully)
+    {
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new JsonStringEnumConverter());
+        return Results.Json((await developer).Message, options);
+    }
+
+    return Results.NotFound(id);
+});
+
+await app.RunAsync();
